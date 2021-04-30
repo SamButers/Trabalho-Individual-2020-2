ff2e8e5953b8a1fd9ab3f25365841057cf48daca
Sam Buters <sambuters@gmail.com>
2021-04-30 20:07:38 -0300

    Adding API container and Postgres container

---
+++ api/start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function_postgres_ready() {
+python << END
+import socket
+import time
+import os
+
+print('cock and ball torture')
+
+port = int(os.environ["POSTGRES_PORT"])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', port))
+s.close()
+END
+}
+
+pip install django djangorestframework psycopg2 django-cors-headers
+
+until function_postgres_ready; do
+  >&2 echo "======= POSTGRES IS UNAVAILABLE, WAITING"
+  sleep 3
+done
+echo "======= POSTGRES IS UP, CONNECTING"
+
+echo '======= RUNNING MIGRATIONS'
+python manage.py migrate
+
+echo '======= RUNNING SERVER'
+python manage.py runserver 0.0.0.0:8000
